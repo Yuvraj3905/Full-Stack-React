8ff694b99eb46123ad7150b94b4862b1ea602a3f
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-21 10:48:32 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
 class App extends Component {
   state = {
     hello: null,
