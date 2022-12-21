e5f13cbe0c0c4039467bc7ca946cea9bc2590384
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-21 10:38:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 class App extends Component {
-  state={
-    hello:null
+  state = {
+    hello: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      // .then(res => this.setState({hello: res.data}))
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   }
 
-componentDidMount(){
-  axios.get('/hello')
-  .then(res => this.setState({hello: res.data}))
-  .catch(err => console.log(err))
-}
-
-render(){
-  return (
-     <div>
-      {this.state.hello
-      ? <div>{this.state.hello}</div>
-    :null }
-     </div>
-    );
-};
+  render() {
+    return <div>{this.state.hello ? <div>{this.state.hello}</div> : null}</div>;
+  }
 }
 
 export default App;
