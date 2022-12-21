3709e7016886a7abafe46837af42f098031d5c2e
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-21 10:23:26 +0530

    Proxy Server and Making API

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import axios from 'axios';
+class App extends Component {
+  state={
+    hello:null
+  }
 
-function App() {
+componentDidMount(){
+  axios.get('/hello')
+  .then(res => this.setState({hello: res.data}))
+  .catch(err => console.log(err))
+}
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+      {this.state.hello
+      ? <div>{this.state.hello}</div>
+    :null }
+     </div>
+    );
+};
 }
 
 export default App;
