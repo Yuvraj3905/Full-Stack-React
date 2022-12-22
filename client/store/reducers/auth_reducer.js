f1f8d2a78e1a51d104029863a750d85b633747d4
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-22 11:02:05 +0530

    Setting Up Reducers

--- client/store/reducers/auth_reducer.js
+++ client/store/reducers/auth_reducer.js
@@ -2,7 +2,8 @@ import * as ACTION_TYPES from '../actions/action_types'
 
 const initialState = {
   is_authenticated: false,
-  profile: null
+  profile: null,
+  db_profile: null
 }
 
 const AuthReducer = (state = initialState, action) => {
@@ -27,6 +28,18 @@ const AuthReducer = (state = initialState, action) => {
           ...state,
           profile: null
         }
+
+        case ACTION_TYPES.SET_DB_PROFILE:
+        return {
+          ...state,
+          db_profile: action.payload
+        }
+      case ACTION_TYPES.REMOVE_DB_PROFILE:
+        return {
+          ...state,
+          db_profile: null
+        }
+        
       default:
         return state
     }
