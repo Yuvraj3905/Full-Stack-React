316f7b3c32e98981cfcd8e1b7fa2753c0eaa4b8f
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-23 20:16:56 +0530

    Update routes.js

--- server/main/routes.js
+++ server/main/routes.js
@@ -57,51 +57,116 @@ router.delete('/api/delete/post', (req, res, next) => {
   });
 });
 
-
 //Comments
 
 router.post('/api/post/commenttodb', (req, res, next) => {
-   const values = [req.body.comment, req.body.user_id, req.body.username, req.body.post_id] 
-   
-   pool.query(`INSERT INTO comments (comment, user_id, author, post_id, date_created)
-    VALUES ($1, $2, $3, $4, NOW())`, values,
-     (q_err, q_res) =>{
-       res.json(qs.rows)
-        console.log(q_err) 
-      }) 
-    }) 
+  const values = [
+    req.body.comment,
+    req.body.user_id,
+    req.body.username,
+    req.body.post_id,
+  ];
+
+  pool.query(
+    `INSERT INTO comments (comment, user_id, author, post_id, date_created)
+    VALUES ($1, $2, $3, $4, NOW())`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
+  );
+});
 
 router.put('/api/put/commenttodb', (req, res, next) => {
-   const values= [req.body.comment, req.body.user_id, req.body.post_id, req.body.username, req.body.c_id] 
-   
-   pool.query(`UPDATE comments SET 
-   comment=$1, user_id=$2, post_id=$3,author=$4, date_created=NOW()
-   WHERE cid=$5`, values, 
-    (q_err, q_res) => {
-       res.json(q_res.rows)
-      console.log(q_err) 
-    })
-   })
-router.delete('/api/delete/commenttodb', (req, res, next) => {
-    const cid=req.body.cid
+  const values = [
+    req.body.comment,
+    req.body.user_id,
+    req.body.post_id,
+    req.body.username,
+    req.body.c_id,
+  ];
 
-    pool.query(`DELETE FROM comments 
-    WHERE cid=$1`, [cid], 
-     (q_err, q_res) => {
-        res.json(q_res.rows)
-       console.log(q_err) 
-     })
-    })
+  pool.query(
+    `UPDATE comments SET 
+   comment=$1, user_id=$2, post_id=$3,author=$4, date_created=NOW()
+   WHERE cid=$5`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
+  );
+});
+router.delete('/api/delete/commenttodb', (req, res, next) => {
+  const cid = req.body.cid;
+
+  pool.query(
+    `DELETE FROM comments 
+    WHERE cid=$1`,
+    [cid],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
+  );
+});
 router.get('/api/delete/commenttodb', (req, res, next) => {
-      const post_id=String(req.query.post_id)
+  const post_id = String(req.query.post_id);
+
+  pool.query(
+    `SELECT * FROM comments 
+      WHERE post_id=$1`,
+    [post_id],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
+  );
+});
+
+// USER PROFILE DATA
+
+router.post('/api/post/userprofiletodb', (req, res, next) => {
+  const values = [
+    req.body.profile.nickname,
+    req.body.profile.email,
+    req.body.profile.email_verified,
   
-      pool.query(`SELECT * FROM comments 
-      WHERE post_id=$1`, [post_id], 
-       (q_err, q_res) => {
-          res.json(q_res.rows)
-         console.log(q_err) 
-       })
-      })
+  ];
+
+  pool.query(
+    `INSERT INTO users (username, email, email_verified, date_created)
+    VALUES ($1, $2, $3, NOW())
+    ON CONFLICT DO NOTHING`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
+  );
+});
+
+router.post('/api/post/userprofiletodb', (req, res, next) => {
+  const values = [
+    req.body.profile.nickname,
+    req.body.profile.email,
+    req.body.profile.email_verified,
+  
+  ];
+
+  pool.query(
+    `INSERT INTO users (username, email, email_verified, date_created)
+    VALUES ($1, $2, $3, NOW())
+    ON CONFLICT DO NOTHING`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+      console.log(q_err);
+    }
+  );
+});
+  
 
 
 
