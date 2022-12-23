a57e3b1b7b4ca76d58ff7b45af55a84acc3d762f
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-23 15:46:40 +0530

    Update routes.js

--- server/main/routes.js
+++ server/main/routes.js
@@ -2,8 +2,26 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/hello', function (req, res) {
-  res.json('Hello World');
-});
+var pool=require('/db');
+
+//Posts Router Section
+
+router.get('/api/get/allposts',(req,res,next)=>{
+  pool.query("SELECT * FROM posts ORDER y date_created DESC", (q_err,q_res)=>{
+    res.json(q_res.rows)
+  })
+})
+ 
+router.posts('/api/posts/poststodb',(req,res,next)=>{
+  const values=[req.body.title, req.body.body , req.body.body.uid, req.body.username ]
+  pool.query("INSERT INTO posts(title, body, user_id,author,date_created) VALUES($1,$2,$3,$4,NOW())", values, (q_err,q_res)=>{
+    if(q_err) return next(q_err);
+    res.json(q_res.rows)
+  })
+})
+
+// router.get('/hello', function (req, res) {
+//   res.json('Hello World');
+// });
 
 module.exports = router;
